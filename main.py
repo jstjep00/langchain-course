e49ce6b3189e26262eb77af895c0cbb0831e2bc6
Josip Stjepanovic <jstjep00@gmail.com>
2025-10-18 02:46:10 +0200

    adding Langsmith traceability

--- main.py
+++ main.py
@@ -4,16 +4,38 @@ from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_community.llms.vllm import VLLMOpenAI
 
+from langsmith import traceable
+
 load_dotenv()
 
 
-def main():
+@traceable
+def template_creation() -> PromptTemplate:
+    summary_template = """
+         given the information {information} I want you to create a:
+         1. short summary about the person
+         2. two interesting facts about the person
+    """
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+    return summary_prompt_template
+
+
+@traceable
+def llm_creation() -> VLLMOpenAI:
     llm = VLLMOpenAI(
         openai_api_key="EMPTY",
         openai_api_base=f"{os.getenv("OPENAI_API_BASE")}",
         model_name=f"{os.getenv("MODEL_NAME")}",
-        max_tokens=1024
+        max_tokens=1024,
     )
+    return llm
+
+
+@traceable
+def main():
+    llm = llm_creation()
 
     information = """Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a businessman and entrepreneur known for his leadership of Tesla, SpaceX, Twitter, and xAI. Musk has been the wealthiest person in the world since 2021; as of October 2025, Forbes estimates his net worth to be US$500 billion.
 
@@ -25,20 +47,14 @@ Musk was the largest donor in the 2024 U.S. presidential election, and is a supp
 
 Musk's political activities, views, and statements have made him a polarizing figure, especially following the COVID-19 pandemic. He has been criticized for making unscientific and misleading statements, including COVID-19 misinformation and promoting conspiracy theories, and affirming antisemitic, racist, and transphobic comments. His acquisition of Twitter was controversial due to a subsequent increase in hate speech and the spread of misinformation on the service. His role in the second Trump administration attracted public backlash, particularly in response to DOGE. 
 """
-    summary_template = """
-         given the information {information} I want you to create a:
-         1. short summary about the person
-         2. two interesting facts about the person
-"""
 
-    summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
-    )
+    summary_prompt_template = template_creation()
 
     chain = summary_prompt_template | llm
     response = chain.invoke(input={"information": information})
-    print(response)
+    return response
 
 
 if __name__ == "__main__":
-    main()
+    response = main()
+    print(response)
